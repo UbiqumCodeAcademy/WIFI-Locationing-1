4c5c03b74d4f8020dab194ba2f3d149623596eed
Carmen <carmen@corvalan.net>
2019-02-19 12:59:45 +0100

    .

--- Wifi Location.R
+++ Wifi Location.R
@@ -11,8 +11,18 @@ pacman:: p_load(pacman,
                 randomForest, 
                 caret, 
                 viridis, 
-                cowplot
+                cowplot,
+                bindrcpp, 
+                doMC
 )
+####Parallel processing####
+#did it work?
+
+getDoParWorkers()
+makeCluster(1)
+registerDoMC(1)
+
+
 #### 2- Load Data Sets ####
 
 td <- read_csv("~/Desktop/Ubiqum/Data Analysis/RStudio/Wifi Location/UJIndoorLoc/trainingData.csv")
@@ -27,38 +37,41 @@ tdwaps <- td [,-c(notworking_waps)]
 
 #### 3- Convert Variables #### 
 
-td$FLOOR <- as.factor(td$FLOOR)
-td$BUILDINGID <- as.factor(td$BUILDINGID)
-td$SPACEID <- as.factor(td$SPACEID)
-td$RELATIVEPOSITION <- as.factor(td$RELATIVEPOSITION)
-td$USERID <- as.factor(td$USERID)
-td$PHONEID <- as.factor(td$PHONEID)
-td$TIMESTAMP <- as.POSIXct(as.numeric(td$TIMESTAMP), origin = '1970-01-01')
+tdwaps$FLOOR <- as.factor(td$FLOOR)
+tdwaps$BUILDINGID <- as.factor(td$BUILDINGID)
+tdwaps$SPACEID <- as.factor(td$SPACEID)
+tdwaps$RELATIVEPOSITION <- as.factor(td$RELATIVEPOSITION)
+tdwaps$USERID <- as.factor(td$USERID)
+tdwaps$PHONEID <- as.factor(td$PHONEID)
+tdwaps$TIMESTAMP <- as.POSIXct(as.numeric(td$TIMESTAMP), origin = '1970-01-01')
 
 
 #### 4- Creating a new column w/ strongest WAP on the row ####
 
-tdwaps[, 1:465] <- as.data.frame(apply(tdwaps[, 1:465], 2, function(x) {ifelse(x==100, -100,x)}))
+tdwaps[, 1:465] <- as.data.frame(apply(
+  tdwaps[, 1:465], 2, function(x) {ifelse(x==100, -100,x)}))
 
 tdwaps <- filter(tdwaps, USERID != 6)
 
 WAP <- grep("WAP", names(tdwaps), value = T)
 
 tdwaps$maxWAP <- apply(tdwaps[WAP], 1, which.max)
-
-tdwaps$lengthwap <- apply(tdwaps [,1:465], 1, function(x) length(x))
+names(tdwaps)
+# zerovariancewaps <- as.data.frame(apply(tdwaps [WAP], 1, 
+#                           nearZeroVar, saveMetrics = TRUE))
+# 
+# wifidt <- tdwaps[, -zerovariancewaps] 
+# I am pretty sure NearZeroVariance doesnt go this way
 
 #### 5- Convert from wide to long for visualization ####
 
-lttd <- melt(td, id.vars = c(521:529))
+lttd <- melt(tdwaps, id.vars = c(466:475))
 traincomplete <-melt(td, id.vars = c(521:529))
-lttd <- lttd[,c(10, 11, 1, 2, 3, 4, 5, 6, 7, 8, 9)]
 lttd <- filter(lttd, value != 100)
 lttd <- filter(lttd, USERID != 6)
-summary(lttd)
+maxwapgrouped <- dplyr::group_by(tdwaps, maxWAP)
 
 test <- melt(vd, id.vars = c(521:529))
-test <-  test[,c(10, 11, 1, 2, 3, 4, 5, 6, 7, 8, 9)]
 test <- filter(test, value != 100)
 
 #### 6- Plots and visualization ####
@@ -85,28 +98,20 @@ ggplot(data = tdwaps) +
   theme_minimal() +
   facet_wrap(vars(BUILDINGID))
 
-plot1 <- plot_ly(train, x= ~LATITUDE, 
+plot1 <- plot_ly(lttd, x= ~LATITUDE, 
                  y = ~LONGITUDE, 
                  z = ~FLOOR, 
-                 marker = list(color = ~ value, 
+                 marker = list(color = ~ FLOOR, 
                                colourscale = "earth", 
-                               showscale = TRUE)) %>%
+                               showscale = FALSE)) %>%
   add_markers()%>%
   layout(scene = list(xaxis = list(title = 'Latitude'),
                       yaxis = list(title = 'Longitude'),
-                      zaxis = list(title = 'Floor')),
-         annotations = list(
-           x = 1.13,
-           y = 1.05,
-           text = 'WAPS Strength ',
-           xref = 'paper',
-           yref = 'paper',
-           showarrow = FALSE
-         ))
+                      zaxis = list(title = 'Floor')))
 
 plot1
 
-plot2 <- plot_ly(closeWAPS, x= ~LATITUDE, 
+plot2 <- plot_ly(maxwaps, x= ~LATITUDE, 
                  y = ~LONGITUDE, 
                  z = ~FLOOR, 
                  marker = list(color = ~ value, 
@@ -126,16 +131,36 @@ plot2 <- plot_ly(closeWAPS, x= ~LATITUDE,
          ))
 plot2
 
-plot(LATITUDE ~ LONGITUDE, data = closeWAPS, pch = 20, col = "cyan")
+plot(LATITUDE ~ LONGITUDE, data = lttd, pch = 20, col = "cyan")
 
-#### 8- Sampling ####
+#### 7- Sampling ####
 
-sample1 <- td %>% group_by(BUILDINGID, FLOOR) %>% sample_n(30)
+# Sampling n=30
+sample1 <- tdwaps %>% group_by(BUILDINGID, FLOOR) %>% sample_n(30)
 
 table(sample1$FLOOR)
 table(sample1$BUILDINGID)
 
+# Filter by building 
 b0 <- filter(sample1, BUILDINGID == 0)
+b1 <- dplyr::filter(sample1, BUILDINGID == 1)
+b2 <- dplyr::filter(sample1, BUILDINGID == 2)
+
+## Plot
+
+sample1$BUILDINGID <- as.character(sample1$BUILDINGID)
+
+a <- htmltools::tagList()
+
+for (i in unique(sample1$BUILDINGID)) {
+  a[[i]] <- sample1 %>% dplyr::filter(BUILDINGID == i)
+  %>% plot_ly(sample1, 
+              x = ~ LONGITUDE, 
+              y = ~ LATITUDE, 
+              z = ~ FLOOR, 
+              type = "scatter3d", 
+              mode = "markers")
+}
 
 plotsample1 <- plot_ly(sample1, x= ~LONGITUDE, 
                  y = ~LATITUDE, 
@@ -144,7 +169,6 @@ plotsample1 <- plot_ly(sample1, x= ~LONGITUDE,
                  mode = "markers", 
                  color = ~BUILDINGID)
 plotsample1
-
 
 plotb0 <- plot_ly(b0, 
                   x= ~LONGITUDE, 
@@ -155,33 +179,79 @@ plotb0 <- plot_ly(b0,
                   color = ~FLOOR)
 plotb0
 
+
+
 #### 8- Modeling ####
 
-wap1s <- grep("WAP", names(sample1), value = T)
+waps<- grep("WAP", names(sample1), value = T)
 
-# Get the best mtry
-besmtry_rf <- tuneRF(sample1[wap1s], 
-                     sample1$LONGITUDE, 
+#### 8.1 FLOOR ####
+# Get the best mtry / PREDICTING FLOOR
+#Random Forest
+besmtry_rf <- tuneRF(sample1[waps], 
+                     sample1$FLOOR, 
+                     ntreeTry = 100, 
+                     stepFactor = 2, 
+                     improve = 0.05,
+                     trace = TRUE, 
+                     plot = T)
+#mtry = 42 	OOB error = 11.79% 
+#0 0.05 
+
+# Train a random forest using that mtry
+system.time(rf1 <- randomForest(y=sample1$FLOOR, 
+                                x=sample1[waps], 
+                                importance = T, 
+                                method ="rf", 
+                                ntree = 100, 
+                                mtry = 42))
+#user  system elapsed 
+#1.560   0.010   1.578 
+
+# Train a random forest using caret package
+system.time(rf1_caret <- train(y=sample1$LONGITUDE, 
+                                      x= sample1[waps], 
+                                      data = sample1, 
+                                      method = "rf", 
+                                      ntree = 100, 
+                                      tuneGrid = expand.grid(.mtry = 42)))
+#  user  system elapsed 
+#9.698   0.283  10.041 
+
+#KNN
+
+
+
+#### 8.2 BUILDING ####
+# Get the best mtry / PREDICTING Building 
+besmtry_rf <- tuneRF(sample1[waps], 
+                     sample1$BUILDINGID, 
                      ntreeTry = 100, 
                      stepFactor = 2, 
                      improve = 0.05,
                      trace = TRUE, 
                      plot = T)
 
+#mtry = 42 	OOB error = 11.79% 
+#0 0.05 
+
 # Train a random forest using that mtry
-system.time(rf1 <- randomForest(y=sample1$LONGITUDE, 
-                                x=sample1[wap1s], 
+system.time(rf1 <- randomForest(y=sample1$FLOOR, 
+                                x=sample1[waps], 
                                 importance = T, 
                                 method ="rf", 
                                 ntree = 100, 
-                                mtry = 44))
+                                mtry = 42))
+#user  system elapsed 
+#1.560   0.010   1.578 
 
 # Train a random forest using caret package
 system.time(rf1_caret <- train(y=sample1$LONGITUDE, 
-                                      x= sample1[wap1s], 
-                                      data = sample1, 
-                                      method = "rf", 
-                                      ntree = 100, 
-                                      tuneGrid = expand.grid(.mtry = 44)))
-
+                               x= sample1[waps], 
+                               data = sample1, 
+                               method = "rf", 
+                               ntree = 100, 
+                               tuneGrid = expand.grid(.mtry = 42)))
+#  user  system elapsed 
+#9.698   0.283  10.041 
 
